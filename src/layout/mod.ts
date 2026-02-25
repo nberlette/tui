1361a413bb4d5705f3df50fd5a9024686b4a4fe6
Nicholas Berlette <nick@berlette.com>
2026-02-24 18:48:41 -0800

    chore: fmt

--- src/layout/mod.ts
+++ src/layout/mod.ts
@@ -1,6 +1,6 @@
 // Copyright 2023 Im-Beast. MIT license.
 export * from "./errors.ts";
+export * from "./grid_layout.ts";
 export * from "./horizontal_layout.ts";
 export * from "./layout.ts";
 export * from "./vertical_layout.ts";
-export * from "./grid_layout.ts";
