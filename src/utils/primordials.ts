57df2f437f13a970f2dbe951fa314ed26d36d9b6
Nicholas Berlette <nick@berlette.com>
2026-03-09 18:29:06 -0700

    chore: fmt

--- src/utils/primordials.ts
+++ src/utils/primordials.ts
@@ -13,15 +13,17 @@ export const $global: typeof globalThis = (() => {
 const $O = $global.Object,
   $defs = $O.defineProperties,
   $gpds = $O.getOwnPropertyDescriptors;
-  // $create = $O.create,
-  // $def = $O.defineProperty,
-  // $gpd = $O.getOwnPropertyDescriptor,
+// $create = $O.create,
+// $def = $O.defineProperty,
+// $gpd = $O.getOwnPropertyDescriptor,
 
 export const SafeObject = $defs($O.bind($O), $gpds($O));
 
 export const ObjectAssign = SafeObject.assign;
 export const ObjectDefineProperty = SafeObject.defineProperty;
 export const ObjectDefineProperties = SafeObject.defineProperties;
-export const ObjectGetOwnPropertyDescriptor = SafeObject.getOwnPropertyDescriptor;
-export const ObjectGetOwnPropertyDescriptors = SafeObject.getOwnPropertyDescriptors;
+export const ObjectGetOwnPropertyDescriptor =
+  SafeObject.getOwnPropertyDescriptor;
+export const ObjectGetOwnPropertyDescriptors =
+  SafeObject.getOwnPropertyDescriptors;
 export const ObjectCreate = SafeObject.create;
